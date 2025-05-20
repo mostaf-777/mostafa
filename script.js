0eb7eddc18a2b5c8b211ce52046993dfefa9a630
mostaf-777 <riri03659@gmail.com>
2025-05-20 22:22:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function j()
+ {
+    let name=document.getElementById("b").value;
+    let message=" اهلا بك يا "+name;
+    document.getElementById("m").innerText=message
+    
+ }   
+function a()
+{
+window.location.href="page2.html"
+
+}
+
+    
